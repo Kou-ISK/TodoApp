f1b65319f1e150dbd40233a2a525b850f3320de5
k-isaka <isk.rugby@gmail.com>
2023-05-29 23:15:37 +0900

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 井坂航 on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
